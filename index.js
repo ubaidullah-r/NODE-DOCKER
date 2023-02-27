0e3c2593bc28fd44e66fa3be6bc56a52cc3c4588
Ubaid Ullah <ubaidullah4211@gmail.com>
2023-02-27 22:49:05 +0500

    added crud api

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require("express")
 const mongoose = require('mongoose');
-
+const {MONGO_USER,MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config.js");
+const postRouter = require("./routes/postRoutes");
 
 
 const app = express();
-mongoose.connect('mongodb://ubaid:ubaid@mongo-db:27017/?authSource=admin')
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`; 
+const connectWithRetry = () => {
+  mongoose.connect(mongoURL)
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.log('Failed to connect to MongoDB', error);
+    setTimeout(connectWithRetry, 5000 )
   });
 
+}
+
+connectWithRetry();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h1>Hi There</h1>")
 })
 
+app.use("/api/v1/posts", postRouter)
 const port = process.env.PORT || 3000
 
 
