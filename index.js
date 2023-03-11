fbb544cd5bf2223f10f628d0e23b878c3c87cf20
Ubaid Ullah <ubaidullah4211@gmail.com>
2023-03-11 16:47:38 +0500

    adding custom image in docker file

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-    res.send("<h1>Hi There</h1>");
+    res.send("<h1>Hi There!!!</h1>");
     console.log("hi there, sweety");
 })
 
